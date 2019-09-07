c75dd6b1e5778954043f1753d26cbb3c9b7e9099
Tomas Ortega <torte007@fiu.edu>
2019-09-07 13:53:18 -0400

    first commit

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings: 
+    """ A class to store all the settings for the alien invasion game"""
+
+    def __init__(self):
+        """ Initialize the game's settings"""
+        # First we go through the screen settings
+        self.screen_width = 1200
+        self.screen_height = 600
+        self.bg_color = (0, 0, 0)
+        self.screen_size = self.screen_width, self.screen_height
