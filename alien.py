79a6bfb06bb60e7a20f3eba1edba587bd3876bdd
Tomas Ortega <torte007@fiu.edu>
2019-09-08 06:30:40 -0400

    adding more aliens

--- alien.py
+++ alien.py
@@ -11,18 +11,19 @@ class Alien(Sprite):
         super().__init__()
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
+        self.settings = ai_game.settings
 
-        # Load the aline image and set its rect attribute. 
-        if random() < 0.3:
-            self.image = pygame.image.load('images/monster-big.bmp')
-        else:
-            self.image = pygame.image.load('images/monster-small.bmp')
+#        # load the aline image and set its rect attribute. 
+#        if random() < 0.3:
+#            self.image = pygame.image.load('images/monster-big.bmp')
+#        else:
+#            self.image = pygame.image.load('images/monster-small.bmp')
+        self.image = pygame.image.load('images/monster-small.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new alien near the top left of the screen.
         self.rect.x = randint(self.screen_rect.left, self.screen_rect.right)
-        self.rect.y = randint(self.screen_rect.top, self.screen_rect.bottom)
-        # Store the alien's exact horizontal position. 
+        self.rect.y = randint(0, self.screen_rect.bottom - self.rect.height)     # Store the alien's exact horizontal position. 
         self.x = float(self.rect.x)
         
 
